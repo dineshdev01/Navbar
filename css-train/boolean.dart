a71bcbbb020537bb0d3f11571a154bcf59515f63
Dinesh-M-India <dineshtechdeveloper@gmail.com>
2022-11-09 15:20:45 +0530

    IO In DART

--- css-train/boolean.dart
+++ css-train/boolean.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
 void main(List<String> args) {
   var Istrue = false;
   print(Istrue.runtimeType);
+  //Dart IO Operations
+  var state,country;
+print("Enter The State");
+  state = stdin.readLineSync();
+
 }
