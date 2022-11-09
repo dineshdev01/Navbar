575d63868b2c247c95e96779e021399f02174e68
Dinesh-M-India <dineshtechdeveloper@gmail.com>
2022-11-09 14:39:16 +0530

    stringbaseoperations

--- string.dart
+++ string.dart
@@ -3,6 +3,12 @@ void main(List<String> args) {
   var v2 ="Tamilnadu";
   print("Hello ${v1} , Welcome ${v2}");
   var puthusu ="Tamilnadu India";
-  
+print(puthusu.substring(0,9));
+var position = puthusu.indexOf(" ");
+var white = puthusu.substring(position).trim();
+var wwhite = puthusu.substring(position);
+print(puthusu.substring(position));
+print(white.length);
+print(wwhite.length);
   
 }
